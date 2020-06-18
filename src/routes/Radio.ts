ed7f39afd28d0796878e647d448b7dc32c90fcf2
1g0rbm <m1g0rb89@gmail.com>
2020-06-18 23:44:23 +0300

    Add navbar + 2 pages

--- src/routes/Radio.ts
+++ src/routes/Radio.ts
@@ -3,7 +3,23 @@ import { Router } from 'express'
 const router: Router = Router()
 
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'kubadroid'
+  })
+})
+
+router.get('/on-air', (req, res) => {
+  res.render('on-air', {
+    title: 'On Air',
+    isOnAir: true
+  })
+})
+
+router.get('/timetable', (req, res) => {
+  res.render('timetable', {
+    title: 'Timetable',
+    isTimeTable: true
+  })
 })
 
 export default router
