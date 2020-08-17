891645deda1533929a49600bb8357bb68abdcd1d
1g0rbm <m1g0rb89@gmail.com>
2020-08-18 00:18:26 +0300

    Mark current menu item as active

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useLocation} from 'react-router-dom'
 import {useDispatch} from "react-redux"
 import {logout} from "../redux/auth/authActions"
+import classnames from "classnames";
 
 export default () => {
+  const location = useLocation()
   const dispatch = useDispatch()
 
   const logoutHandler = event => {
@@ -12,12 +14,16 @@ export default () => {
   }
 
   return (
-    <nav >
+    <nav>
       <div className="nav-wrapper blue lighten-2">
         <span className="brand-logo">Kubadroid</span>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><NavLink to="/news">News</NavLink></li>
-          <li><NavLink to="/programs">Programs</NavLink></li>
+          <li className={classnames({active: location.pathname === '/news'})}>
+            <NavLink to="/news">News</NavLink>
+          </li>
+          <li className={classnames({active: location.pathname === '/broadcasts'})}>
+            <NavLink to="/broadcasts">Programs</NavLink>
+          </li>
           <li><a href="/" onClick={logoutHandler}>Logout</a></li>
         </ul>
       </div>
